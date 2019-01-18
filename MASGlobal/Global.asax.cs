547bc49d1b8651d6c8ee6f4156acefa67a267c9f
Alejandro Alvarez <alealvarezarg@gmail.com>
2019-01-18 11:46:08 -0500

    Base Project 1

--- MASGlobal/Global.asax.cs
+++ MASGlobal/Global.asax.cs
@@ -10,9 +10,7 @@ namespace MASGlobal
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             UnityConfig.RegisterComponents();
-
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
